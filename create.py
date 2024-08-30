b534085ea346fc6751b129edc5c0da19b58d132d
Linus <khabibtime@rambler.ru>
2024-08-30 21:51:14 +0300

    add

--- create.py
+++ create.py
@@ -1,10 +1,34 @@
 import os
+import venv
+from virtualenv import cli_run
+
+with open("run.py", "w") as file:
+    pass
+
+cli_run(["venv"])
+
+os.mkdir("app")
+os.chdir("app")
 
 
+with open("__init__.py", "w") as file:
+    text = """from flask import Flask
+
+app = Flask(__name__)"""
+    file.write(text)
+
+with open("models.py", "w") as file:
+    pass
+
+with open("config.py", "w") as file:
+    text = """class Config:
+    SECRET_KEY = "12345"
+"""
+
+    file.write(text)
 
 def main():
     pass
-
 
 if __name__ == "__main__":
     main()
